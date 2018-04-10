041538f974ca7f836454cb5df3a83abc054e3590
reeyashah <35962907+reeyashah@users.noreply.github.com>
2018-04-10 10:04:37 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,13 +1,49 @@
-#model.py
 import csv
+BB_FILE_NAME = "umbball.csv"
+bb_season = []
 
-BB_FILE_NAME = 'umbball.csv'
 
-bb_seasons = []
+def init_bball(csv_file_name=BB_FILE_NAME):
+    global bb_seasons
+    with open(csv_file_name) as f:
+        reader = csv.reader(f)
+        next(reader) # throw away headers
+        next(reader) # throw away headers
+        global bb_seasons
+        bb_seasons = [] # reset, start clean
+        for r in reader:
+            r[3] = int(r[3])
+            r[4] = int(r[4])
+            r[5] = float(r[5])
+            bb_seasons.append(r)
 
-def init_bball(csv_file_name):
-    pass
 
 def get_bball_seasons(sortby='year', sortorder='desc'):
-    global bb_seasons
-    return bb_seasons
+    if sortby == 'year':
+        sortcol = 1
+    elif sortby == 'wins':
+        sortcol = 3
+    elif sortby == 'pct':
+        sortcol = 5
+    else:
+        sortcol = 0
+
+    rev = (sortorder == 'desc')
+    sorted_list = sorted(bb_seasons, key=lambda row: row[sortcol], reverse=rev)
+    return sorted_list
+
+@app.route('/bball', methods=['GET', 'POST'])
+def bball():
+     if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_bball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_bball_seasons()
+
+    return render_template("seasons.html", seasons=seasons)
+
+
+if __name__ == '__main__':
+    model.init_bball()
+    app.run(debug=True)
